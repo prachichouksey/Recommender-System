4074de6fda3e66487b57f3278c0c8b411a828061
Ruchika Hazariwal <ruchikahazariwal17@gmail.com>
2019-07-16 17:30:37 -0700

    data

--- web_scrap.py
+++ web_scrap.py
@@ -331,7 +331,7 @@ class Search:
 if __name__ == '__main__':
     import pandas
     from openpyxl import load_workbook
-    s = Search('LiFi',results_limit=400).as_dataframe()
+    s = Search('Microblogging',results_limit=500).as_dataframe()
     writer = pandas.ExcelWriter('test4.xlsx', engine='openpyxl')
     writer.book = load_workbook('test4.xlsx')
     writer.sheets = {ws.title: ws for ws in writer.book.worksheets}
