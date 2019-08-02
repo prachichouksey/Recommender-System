6f74b661545b638e439c1dae31d76908dd02c797
prachichouksey <prachi.chouksey@sjsu.edu>
2019-08-01 17:37:26 -0700

    user id integration

--- web_app/app.py
+++ web_app/app.py
@@ -1,3 +1,8 @@
+"""
+Created on Tue Jul  23 17:02:27 2019
+
+@author: prach
+"""
 from flask import Flask, render_template, request
 import pandas as pd
 import main
@@ -12,33 +17,33 @@ def home():
 def recommender():
     keyword = str(request.form['keyword'])
     searchCriteria = str(request.form['searchCriteria'])
-#    try:
-    if(searchCriteria=="keyword"):
-        result=main.patentKeywordMatch(keyword)
-    elif(searchCriteria=="userid"):
-        result=main.patentUserIdMatch(keyword)
-    elif(searchCriteria=="patentid"):
-        result=main.patentPatentIdMatch(keyword)
-    else:
-        return render_template('index.html',
+    try:
+        if(searchCriteria=="keyword"):
+            result=main.patentKeywordMatch(keyword)
+        elif(searchCriteria=="userid"):
+            result=main.patentUserIdMatch(keyword)
+        elif(searchCriteria=="patentid"):
+            result=main.patentPatentIdMatch(keyword)
+        else:
+            return render_template('index.html',
                            msg="Please select a valid criteria.",
                            form_action='/recommender#search')
-    if not result.empty:
-        if(searchCriteria=="patentid"):
-            result_mypatent=result[:1]
-            result=result[1:]
-            return render_template('recommender.html', msg="", df=result,patent="true",df_mypatent=result_mypatent,form_action="/recommender#search")
+        if not result.empty:
+            if(searchCriteria=="patentid"):
+                result_mypatent=result[:1]
+                result=result[1:]
+                return render_template('recommender.html', msg="", df=result,patent="true",df_mypatent=result_mypatent,form_action="/recommender#search")
+            else:
+                return render_template('recommender.html', msg="", df=result,form_action="/recommender#search")
         else:
-            return render_template('recommender.html', msg="", df=result,form_action="/recommender#search")
-    else:
-        return render_template('index.html',
+            return render_template('index.html',
                                    msg="Unfortunately, this patent is not in our database. Please try another paper.",
                                    form_action='/recommender#search')
 
-#    except:
-#        return render_template('index.html',
-#                           msg="Please enter valid search values.",
-#                           form_action='/recommender#search')
+    except:
+        return render_template('index.html',
+                           msg="Please enter valid search values.",
+                           form_action='/recommender#search')
 
 
 
--- web_app/main.py
+++ web_app/main.py
@@ -1,8 +1,12 @@
+"""
+Created on Tue Jul  23 17:02:27 2019
+
+@author: prach
+"""
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
-
 patentlist = pd.read_csv('../data/Dataset.csv')
 
 patentlist.head()
@@ -55,7 +59,7 @@ def patentKeywordMatch(keyword):
     abstract_similar = pd.Series(indices.flatten()).map(patentlist.reset_index()['abstract'])
     url_similar = pd.Series(indices.flatten()).map(patentlist.reset_index()['url'])
     title_similar = pd.Series(indices.flatten()).map(patentlist.reset_index()['title'])
-    result = pd.DataFrame({'distance':distances.flatten(), 'patent_number':names_similar, 'abstract':abstract_similar, 'url':url_similar,
+    result = pd.DataFrame({'distance':distances.flatten(), 'patent_num':names_similar, 'abstract':abstract_similar, 'url':url_similar,
                       'title':title_similar})
     result=result.sort_values('distance')
     result.to_csv('result_keyword.csv')
@@ -100,12 +104,88 @@ def patentPatentIdMatch(keyword):
     abstract_similar = pd.Series(indices.flatten()).map(patentlist.reset_index()['abstract'])
     url_similar = pd.Series(indices.flatten()).map(patentlist.reset_index()['url'])
     title_similar = pd.Series(indices.flatten()).map(patentlist.reset_index()['title'])
-    result = pd.DataFrame({'distance':distances.flatten(), 'patent_number':names_similar, 'abstract':abstract_similar, 'url':url_similar,
+    result = pd.DataFrame({'distance':distances.flatten(), 'patent_num':names_similar, 'abstract':abstract_similar, 'url':url_similar,
                       'title':title_similar})
     result=result.sort_values('distance')
     result.to_csv('result_patentid.csv')
     
     return result
+
+def get_patentnumbers(id,df):
+    subsetDataFrame = df['PATENT NUMBER'][df['UID'] == id].str.split(",", expand = True) 
+    return subsetDataFrame
+
+
+def get_similarity_matrix(k):
+    numbers = k[0]
+    for i in range(0, len(numbers)): 
+        numbers[i] = int(numbers[i])
+    len(numbers)
+    data= pd.read_csv('similarity_matrix.csv')
+    data.rename( columns={'Unnamed: 0':'patent_no'}, inplace=True)
+    data.set_index('patent_no', inplace=True)
+
+    results = list(data.columns.values) 
+    results
+
+    pp= data.loc[numbers]
+    pp= pp.dropna()
+    pp        
+
+    demo=[]
+    comb=[]
+    for i in range(0,len(numbers)-1):
+        for j in range(0,len(results)):
+            comb=[]
+            if pp.iloc[i][j]!=0.0:
+                comb.append(pp.iloc[i][j])
+                comb.append(results[j])
+                demo.append(comb)
+
+##Sort patents by similarity
+    arr = sorted(demo,reverse=True)[:30]
+    arr1=[]
+    flag=False
+    for i in range(0, len(arr)):
+        flag=False
+        for j in range(0,len(arr1)):
+            if arr[i][1]==arr1[j][1]:
+                flag=True
+        if(flag==False):
+            arr1.append(arr[i])
+    arr1=arr1[:6]
+    
+    patentlist = pd.read_csv('../data/Dataset.csv')
+    patent1 = patentlist[patentlist['patent_num'] == str(arr1[0][1])]
+    patent1['distance']=arr1[0][0]
+    patent2 = patentlist[patentlist['patent_num'] == str(arr1[1][1])]
+    patent2['distance']=arr1[1][0]
+    patent3 = patentlist[patentlist['patent_num'] == str(arr1[2][1])]
+    patent3['distance']=arr1[2][0]
+    patent4 = patentlist[patentlist['patent_num'] == str(arr1[3][1])]
+    patent4['distance']=arr1[3][0]
+    patent5 = patentlist[patentlist['patent_num'] == str(arr1[4][1])]
+    patent5['distance']=arr1[4][0]
+    
+    bigdata1 = pd.concat([patent1, patent2,patent3,patent4,patent5], sort =False)
+    bigdata=bigdata1[['patent_num','abstract','distance','title','url']]
+    bigdata.to_csv('result_user.csv')
+    return bigdata
+
+def patentUserIdMatch(id):
+    df= pd.read_excel('UID_keyword_ptnum.xlsx')
+    df =df.dropna()
+    df
+    user_list = df["UID"].tolist() 
+    user_list
+    if id in user_list:
+        print("USERID is found")
+        k = get_patentnumbers(id,df).values.tolist()
+        res = get_similarity_matrix(k)
+        return res
+    else:
+        print("USERID not there")
+
     
 if __name__=="__main__":
     test=patentKeywordMatch("cloud")
